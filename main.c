3d0a53198d6f1f40e3dbc72ea7fc324bb52d310d
GabrielAlmeidaCastro <d2025008728@unifei.edu.br>
2025-05-12 12:35:48 +0000

    atual

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include "ecop11a.h"
 
-int(main)void
+int main(void)
 {
     printf("Olá UNIFEI\n");
+
+    float volume = esfera(3.0f);
+    printf("Volume da esfera: %.2f\n", volume);
+
     return 0;
     
 }
